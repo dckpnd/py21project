786b2711f8a7637ad4d557832f7555dc9bd0460c
Stasy-ku <90251209+Stasy-ku@users.noreply.github.com>
2022-05-04 01:50:43 +0300

    Update first_part.py

--- first_part.py
+++ first_part.py
@@ -9,12 +9,12 @@ with open("slovar.txt", "r", encoding="utf-8") as file:
             patt = re.compile('1\)')
             word_list = re.split(patt, words, maxsplit=0)
             if len(word_list) > 1:
-                key_word = word_list[0]
-                value_word = " — 1)" + word_list[1]
+                key_word = word_list[0][:-1]
+                value_word = "— 1)" + word_list[1]
                 patt_2 = re.compile('2\)')
                 word_list_2 = re.split(patt_2, value_word, maxsplit=0)
-                value_word_1 = word_list_2[0]
-                value_word_2 = " — 2)" + word_list_2[1]
+                value_word_1 = word_list_2[0][:-2]
+                value_word_2 = "— 2)" + word_list_2[1][:-1]
                 # print(value_word_1)
                 # print(value_word_2)
                 dict_word.setdefault(key_word, []).append(value_word_1)
@@ -47,8 +47,33 @@ if choose_language == "2":
             # print(value)
             with colors.pretty_output(colors.FG_BLUE, colors.BOLD) as out:
                 out.write(find_word)
-
-                if "/" in value:
+                if "1)" in value:
+                    if "/" in value:
+                        sep_words = re.split(" — ", value, maxsplit=0)
+                        with colors.pretty_output(colors.BOLD) as out_1:
+                            out_1.write(sep_words[0])
+                            with colors.pretty_output(colors.BG_GREEN) as out_2:
+                                separ_words = sep_words[1].split(";")
+                                out_2.write(separ_words[0])
+                                out_2.write(separ_words[1][1:])
+                                pr_comp = " ".join(separ_words[2:])
+                                if len(separ_words) > 2 and "ср." not in value:
+                                    out_2.write(pr_comp)
+                                elif len(separ_words) > 2 and "ср." in value:
+                                    print(pr_comp.split("ср.")[0][1:])
+                                    print(separ_words[-1][1:])
+                    else:
+                        separ_words = value.split(";")
+                        with colors.pretty_output(colors.BG_GREEN) as out_1:
+                            out_1.write(separ_words[0])
+                            out_1.write(separ_words[1][1:])
+                            pr_comp = " ".join(separ_words[2:])
+                            if len(separ_words) > 2 and "ср." not in value:
+                                out_1.write(pr_comp)
+                            elif len(separ_words) > 2 and "ср." in value:
+                                print(pr_comp.split("ср.")[0][1:])
+                                print(separ_words[-1][1:])
+                elif "/" in value:
                     sep_words = re.split(" — ", value, maxsplit=0)
                     with colors.pretty_output(colors.BOLD) as out_1:
                         out_1.write(sep_words[0])
@@ -58,7 +83,8 @@ if choose_language == "2":
                             with colors.pretty_output(colors.BG_GREEN) as out_2:
                                 out_2.write(separ_words[0])
                                 if "ср." not in value:
-                                    print(separ_words[1:])
+                                    for items in separ_words[1:]:
+                                        print(items[1:])
                                 else:
                                     pr_comp = " ".join(separ_words[1:])
                                     comp = pr_comp.split("ср.")
@@ -73,7 +99,8 @@ if choose_language == "2":
                     with colors.pretty_output(colors.BG_GREEN) as out_2:
                         out_2.write(separ_words[0])
                         if "ср." not in value:
-                            print(separ_words[1:])
+                            for items in separ_words[1:]:
+                                print(items[1:])
                         else:
                             pr_comp = " ".join(separ_words[1:])
                             comp = pr_comp.split("ср.")
